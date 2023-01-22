d2ec9d3b54ad60ba4038adbccabcbf492572a88f
derStax <kevinhart1001001@gmail.com>
2023-01-22 21:23:30 +0100

    Player and Enemy added

---
+++ Assets/EnemyController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+    [SerializeField] private float _movementSpeed;
+    [SerializeField] private int _damage;
+
+    private Transform _playerTransform;
+    private Rigidbody2D _rb;
+    private void Start() {
+        _playerTransform = FindObjectOfType<PlayerController>().transform;
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    private void FixedUpdate() {
+        float xMove = 1f;
+        if(_playerTransform.position.x < transform.position.x) {
+            xMove = -1;
+        }
+        float yMove = 1f;
+        if(_playerTransform.position.y < transform.position.y) {
+            yMove = -1;
+        }
+        _rb.velocity = new Vector2(xMove * _movementSpeed, yMove * _movementSpeed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        if(collision.gameObject.CompareTag("Player")) {
+            collision.gameObject.GetComponent<PlayerController>().DoDamage(_damage);
+        }
+    }
+}
